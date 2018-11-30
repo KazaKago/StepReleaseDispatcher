e345fee2deefafd82e2d0f7120884bb6bd242fed
Kensuke Tamura <tamura_k@1923.co.jp>
2018-11-30 12:14:05 +0900

    refactoring codes.

--- server/src/main/kotlin/com/kazakago/stepreleasedispatcher/server/job/StepReleaseJob.kt
+++ server/src/main/kotlin/com/kazakago/stepreleasedispatcher/server/job/StepReleaseJob.kt
@@ -1,40 +1,22 @@
 package com.kazakago.stepreleasedispatcher.server.job
 
-import com.kazakago.stepreleasedispatcher.config.ConfigLoader
-import com.kazakago.stepreleasedispatcher.notifier.NotificationProvider
-import com.kazakago.stepreleasedispatcher.notifier.NotificationProviders
 import com.kazakago.stepreleasedispatcher.releasedispatcher.ReleaseDispatcher
+import com.kazakago.stepreleasedispatcher.server.holder.NotificationProvidersHolder
+import com.kazakago.stepreleasedispatcher.server.holder.ReleaseDispatcherHolder
 import org.quartz.Job
 import org.quartz.JobExecutionContext
 
 class StepReleaseJob : Job {
 
-    private val notificationProviders = initializeNotificationProvider()
-    private val config = ConfigLoader.load()
-
     override fun execute(context: JobExecutionContext) {
         try {
-            val stepReleaseDispatcher = ReleaseDispatcher(
-                applicationName = config.applicationName,
-                packageName = config.packageName,
-                p12File = ConfigLoader.getP12KeyFile(),
-                serviceAccountEmail = config.serviceAccountEmail,
-                userFractionSteps = config.userFractionStep
-            )
-            when (val result = stepReleaseDispatcher.executeStepRelease()) {
-                is ReleaseDispatcher.StepReleaseResult.UpdatedTrack -> notificationProviders.postExpansionMessage(result.newTrack, result.oldTrack)
-                is ReleaseDispatcher.StepReleaseResult.NoUpdatedTrack -> notificationProviders.postNoExpansionMessage(result.currentTrack)
+            when (val result = ReleaseDispatcherHolder.INSTANCE.executeStepRelease()) {
+                is ReleaseDispatcher.StepReleaseResult.UpdatedTrack -> NotificationProvidersHolder.INSTANCE.postExpansionMessage(result.newTrack, result.oldTrack)
+                is ReleaseDispatcher.StepReleaseResult.NoUpdatedTrack -> NotificationProvidersHolder.INSTANCE.postNoExpansionMessage(result.currentTrack)
             }
         } catch (exception: Exception) {
-            notificationProviders.postErrorMessage(exception)
+            NotificationProvidersHolder.INSTANCE.postErrorMessage(exception)
         }
-    }
-
-    private fun initializeNotificationProvider(): NotificationProviders {
-        return NotificationProviders(
-            applicationName = config.applicationName,
-            slackType = NotificationProvider.Type.Slack(config.slackWebHookUrl)
-        )
     }
 
 }
